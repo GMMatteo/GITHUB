45e88acdd6cc3e5298ff4afa56d5baa99964675e
Giacomo Matteo <gmatteo@outlook.com>
2021-10-09 11:25:34 -0400

    Updated Randomizer

--- DojoPizzeria.js
+++ DojoPizzeria.js
@@ -1,16 +1,3 @@
-var pizza = {
-    crustType:    ["thick", "deep dish", "hand tossed"],
-    sauceType:  ["marinara", "white", "traditional"],
-    cheeses:   ["mozzarella","feta"],
-    toppings: ["pepperoni", "sausage", "olives", "hot peppers", "mushrooms", "onions"],
-    // display: function(){
-    //     console.log("The crust is:     " + this.crustType);
-    //     console.log("The sauce are:    " + this.sauceType);
-    //     console.log("The cheese is:    " + this.cheeses);
-    //     console.log("The toppings are: " + this.toppings);
-    // }
-};
-    
 function pizzaOven(crustType, sauceType, cheeses, toppings) {
     var pizza = {};
     pizza.crustType = crustType;
@@ -20,19 +7,73 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
     return pizza;
 }
 
-function randomPizza(crustType, sauceType, cheeses, toppings) {
-    var pizza = {};
-    pizza.crustType = crustType;
-    pizza.sauceType = sauceType;
-    pizza.cheeses = cheeses;
-    pizza.toppings = toppings;
-    return pizza [Math.floor(Math.random() * pizza.length)];
+var crustType = [
+    "deep dish",
+    "hand tossed",
+    "thin",
+    "gluten free",
+    "thick"
+];
+
+var sauceType = [
+    "traditional",
+    "marinara",
+    "spicy marinara",
+    "white",
+    "no sauce",
+    "fresh tomatoes"
+];
+
+var cheeses = [
+    "mozzarella",
+    "feta",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "buffalo mozzarella", 
+    "no cheese"
+];
+
+var toppings = [
+    "pepperoni", 
+    "sausage", 
+    "olives", 
+    "hot peppers", 
+    "mushrooms", 
+    "onions", 
+    "bbq chicken", 
+    "pineapple", 
+    "ham"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
 }
-    
-var p1 = pizzaOven("thin", "marinara", "mozzarella", ["pepperoni", "sausage", "hot peppers"]);
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustType);
+    pizza.sauceType = randomPick(sauceType);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
 console.log(p1);
 
-var p2 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
 console.log(p2);
 
 var p3 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
@@ -44,7 +85,7 @@ console.log(p4);
 var p5 = pizzaOven("thin", "marinara", "mozzarella", "hot peppers");
 console.log(p5);
 
-// var rp1 = randomPizza();
-// console.log(rp1);
+var p6 = pizzaOven("thin", "marinara", "mozzarella", ["pepperoni", "sausage", "hot peppers"]);
+console.log(p6);
 
-// console.log(pizza);
+console.log(randomPizza());
